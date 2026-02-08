a766849b16fd9cb4d0933246e8680585ea5ce33e
moustapha <moustapha.dev03@gmail.com>
2026-02-08 13:27:30 +0100

    [FIX] - temporary removed spring security and cors configuration

--- src/main/java/com/mousty/convify_api/controller/YouTubeRestController.java
+++ src/main/java/com/mousty/convify_api/controller/YouTubeRestController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/v1")
 @Tag(name = "YouTube Converter", description = "API for converting YouTube videos")
 public class YouTubeRestController {
 
